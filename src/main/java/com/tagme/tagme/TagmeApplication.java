4c1451a882b61d27d62aaf35275c6c77bccc7a2c
Gael Yasunari Ishimi <yasunarishimi@gmail.com>
2025-10-17 13:25:27 +0200

    initial commit

---
+++ src/main/java/com/tagme/tagme/TagmeApplication.java
@@ -0,0 +1,13 @@
+package com.tagme.tagme;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TagmeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TagmeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tagme/tagme/TagmeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tagme.tagme;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TagmeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
